a56b81ddfc73f96ebf0e4ccadd63e4b823808501
WallingDown <1532147890@qq.com>
2020-08-14 21:42:32 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Demo from './components/5.hoooks/index'
-
+import Index from './components/2.fragment/index'
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Index/>
         <Demo/>
       </div>
     )
---
+++ src/components/2.fragment/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>我是fragment，用我不会增加多余的结构</h1>
+                <h2>相当于Vue中的template标签</h2>
+            </Fragment>
+        )
+    }
+}
